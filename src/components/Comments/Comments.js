6bb057ad30560daba1ab2437962ec9013dd3a910
natecheney-dev <natecheney@gmail.com>
2021-09-07 16:55:08 -0500

    finished

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,10 +5,18 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(item => {
+  
+      return <Comment
+        key = {item.id}
+        username = {item.username}
+        text = {item.text}
+
+        />
+    })}
     </div>
   );
 };
